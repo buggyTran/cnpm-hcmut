a6303b11666b836a7a786e3f149ae95802c99900
thun <tung.vudaothanh@hcmut.edu.vn>
2025-11-17 20:23:55 +0700

    fix

--- client/src/app/router/AppRouter.jsx
+++ client/src/app/router/AppRouter.jsx
@@ -2,15 +2,32 @@ import { Route, Routes } from 'react-router-dom'
 import HomePage from '../../features/home/HomePage'
 import MainLayout from '../../components/layout/MainLayout'
 import LoginPage from '@/features/login/LoginPage'
-import ProfilePage from '@/features/profile/pages/ProfilePage';
-//E:\math 243\CNPM\Tutor-Support-System\client\src\.jsx
+import ProfilePage from '@/features/profile/pages/ProfilePage'
+import ProgramPage from '../../features/program/ProgramPage'
+import ProtectedRoute from './ProtectedRouter'
+import ExplorePage from '../../features/explore/ExplorePage.jsx';
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
-      </Route>
-      <Route path='/login' element={<LoginPage />}>
+        <Route path="programs" element={<ProgramPage />} />
+        <Route 
+          path="explore" 
+          element={
+            <ProtectedRoute>
+              <ExplorePage />
+            </ProtectedRoute>
+          } 
+        />
+        <Route
+          path="profile"
+          element={
+            <ProtectedRoute>
+              <ProfilePage />
+            </ProtectedRoute>
+          }
+        />
 
       </Route>
 
