ab2b96203840ac754324d0771169d6c92eaedc74
Fluoxetines <dat.nguyenshiesuta@hcmut.edu.vn>
2025-11-27 20:30:11 +0700

    add get-subject and create-subject

--- server/src/router/admin.router.ts
+++ server/src/router/admin.router.ts
@@ -1,4 +1,11 @@
-import { createSemester, getAllUser, updateUserStatus } from '@/controller/admin.controller'
+import { 
+    createSemester, 
+    getAllUser, 
+    updateUserStatus, 
+    createSubject,
+    getAllSubject
+} from '@/controller/admin.controller'
+import { get } from 'axios'
 import express from 'express'
 
 const router = express.Router()
@@ -7,5 +14,7 @@ const router = express.Router()
 router.post('/create-semester', createSemester)
 router.get('/get-all-user', getAllUser)
 router.patch('/update-user-status/:id', updateUserStatus)
+router.post('/create-subject/:id', createSubject)
+router.get('/get-all-subject/:id', getAllSubject)
 
 export default router
